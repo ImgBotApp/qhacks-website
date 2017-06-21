194b85cdf88ddda59cfebfd9e08175e02a436ec5
Enoch Tam <me@enocht.am>
2017-06-21 14:11:06 -0400

    Moving clouds and twinkling stars

--- js/main.js
+++ js/main.js
@@ -1,27 +1,31 @@
 
 $(document).ready(function(){
 
-	$(".thumbnail").click(function(){
-		$(this).find(".bio").fadeIn();
-	});
+	/**
+	 * Place stars on page
+	 */
+	var star_container = $('#scenery_intro .star_container');
+	var positions = [ // use preset locations that look good but still sortof random.
+		{ top: 4, left: 30 },{ top: 7, left: 36 },{ top: 9, left: 5 },{ top: 12, left: 95 },{ top: 8, left: 90 },{ top: 14, left: 93 },{ top: 10, left: 12 },{ top: 15, left: 20 },{ top: 40, left: 15 },{ top: 23, left: 12 },{ top: 28, left: 28 },{ top: 31, left: 34 },{ top: 31, left: 8 },{ top: 32, left: 87 },{ top: 38, left: 65 },{ top: 46, left: 30 },{ top: 58, left: 34 },{ top: 55, left: 18 },{ top: 54, left: 80 },{ top: 60, left: 28 },{ top: 67, left: 94 },{ top: 69, left: 13 },{ top: 64, left: 40 },
+		{ top: 68, left: 56 },{ top: 7, left: 65 },{ top: 13, left: 67 },{ top: 45, left: 98 },{ top: 34, left: 79 },{ top: 24, left: 66 },{ top: 45, left: 84 },{ top: 76, left: 58 },{ top: 77, left: 45 },{ top: 16, left: 43 },{ top: 60, left: 68 }
+	]
+	positions.forEach(function(p) {
+		var star = $('<img src="img/star.png" class="star">');
+		var twinkle_period = Math.floor(Math.random() * 4) + 2; // subtle star twinkling effect
 
-	$(".thumbnail .close, .thumbnail .bio").click(function(event){
-	    event.stopPropagation();
-  		event.preventDefault();
-		$(".thumbnail .bio").fadeOut();
-		return false;
-	});
-
-	$(".thumbnail .bio div.tehactualbio").click(function(event){
-	    event.stopPropagation();
-  		event.preventDefault();
-		return false;
-	});
+		star.css({
+			top: p.top + '%',
+			left: p.left + '%',
+			'animation-duration': twinkle_period + 's'
+		});
+		star_container.append(star)
+	}); 
 
 
-	// setup clouds
+	/**
+	 * Setup clouds and animation
+	 */
 	function setupCloudAnimation(cloud){
-
 		var randVal = Math.floor(Math.random() * 100000) + 1;
 		cloud.animate({
 			left: "+=130%"
@@ -46,70 +50,25 @@ $(document).ready(function(){
 		});
 	}
 
-	// setup fade in
+	var clouds_container = $('#scenery_intro .cloud_container')
 
-	var tower = $("#scenery_intro .clock_tower");
-	var buildings = $("#scenery_intro .buildings");
-	var hillLeft = $("#scenery_intro .hill_left");
-	var hillRight = $("#scenery_intro .hill_right");
-	var logoAndTitle = $("#scenery_intro #header-title");
-	var sceneryIntro = $("#scenery_intro");
+	var clouds = [
+		{ width: '130px', top: '8%' },
+		{ width: '200px', top: '30%' },
+		{ width: '250px', top: '15%' },
+		{ width: '180px', top: '23%' }
+	]
+	clouds.forEach(function(sizes){
+		var cloud = $('<img src="img/cloud.png" class="cloud">')
+		var perc = Math.floor(Math.random() * 100) + 1;
+		cloud.css({ left: perc + "%", width: sizes.width, top: sizes.top });
 
-	var cloud1 = $('<img class="cloud cloud_1" src="img/cloud.png">');
-	var cloud2 = $('<img class="cloud cloud_2" src="img/cloud.png">');
-	var cloud3 = $('<img class="cloud cloud_3" src="img/cloud.png">');
-	var cloud4 = $('<img class="cloud cloud_4" src="img/cloud.png">');
-	var clouds = [cloud1,cloud2,cloud3,cloud4];
-
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-		// hillLeft.removeClass('hidden');
-		// hillRight.removeClass('hidden');
-		// tower.removeClass('hidden');
-		// buildings.removeClass('hidden');
-		// logoAndTitle.removeClass('hidden');
-
-		clouds.forEach(function(c){
-			c.remove();
-		});
-	}else{
-		// hillLeft.fadeIn(1200).removeClass('hidden');
-		// hillRight.fadeIn(800).removeClass('hidden');
-		// tower.fadeIn(1600).removeClass('hidden');
-		// buildings.fadeIn(1600).removeClass('hidden');
-		// logoAndTitle.fadeIn(1800).removeClass('hidden');
-
-		// clouds.forEach(function(c){
-		// 	var perc = Math.floor(Math.random() * 100) + 1;
-		// 	console.log(perc) 
-		// 	c.css("left",perc+"%");
-
-		// 	sceneryIntro.append(c);
-		// 	c.fadeIn(1800).removeClass('hidden');
-		// 	setupCloudAnimation(c);
-		// });
-	}
-
-	// setup scroll buttons
-	// $("#btn_sponsorship").click(function(){
-	// 	$("html, body").animate({ scrollTop: $('#sponsor_header').offset().top-20 }, 600);
-	// });
-
-/*
-	$("#signup_to_be_emailed").click(function(){
-		console.log("sdf");
-		$("#subscription_overlay .signup_container .sendgrid-subscription-widget input[type=submit]")
-			.addClass("btn btn_main orange_btn main_btn");
-		$("#subscription_overlay").fadeIn();
+		clouds_container.append(cloud);
+		cloud.fadeIn(1800).removeClass('hidden');
+		setupCloudAnimation(cloud);
 	});
-	$("#subscription_overlay").click(function(){
-		$("#subscription_overlay").fadeOut();
-	});
-	$("#subscription_overlay .signup_container").click(function(e) {
-	   e.stopPropagation();
-	})
-*/
 
-	// hide mlh badge
+	// hide mlh badge when scrolling
 	var hidden = false;
 	$(document).scroll(function(){
 	    if($(this).scrollTop()>=$('#about_header').position().top){
@@ -124,11 +83,6 @@ $(document).ready(function(){
 	        }
 	    }
 	});
-
-	$(function () {
-  		$('[data-toggle="tooltip"]').tooltip()
-	});
-
 
 });
 
