2b20b67170d4768b0c518b970fe08e77ed626fb2
Robert Saunders <robert.saunders@ibm.com>
2017-06-24 12:24:17 -0400

    modifying star locations and gradient z-index

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ $(document).ready(function(){
 	 */
 	var star_container = $('#scenery_intro .star_container');
 	var positions = [ // use preset locations that look good but still sortof random.
-		{ top: 4, left: 30 },{ top: 7, left: 36 },{ top: 9, left: 5 },{ top: 12, left: 95 },{ top: 8, left: 90 },{ top: 14, left: 93 },{ top: 10, left: 12 },{ top: 15, left: 20 },{ top: 40, left: 15 },{ top: 23, left: 12 },{ top: 28, left: 28 },{ top: 31, left: 34 },{ top: 31, left: 8 },{ top: 32, left: 87 },{ top: 38, left: 65 },{ top: 46, left: 30 },{ top: 58, left: 34 },{ top: 55, left: 18 },{ top: 54, left: 80 },{ top: 60, left: 28 },{ top: 67, left: 94 },{ top: 69, left: 13 },{ top: 64, left: 40 },
-		{ top: 68, left: 56 },{ top: 7, left: 65 },{ top: 13, left: 67 },{ top: 45, left: 98 },{ top: 34, left: 79 },{ top: 24, left: 66 },{ top: 45, left: 84 },{ top: 76, left: 58 },{ top: 77, left: 45 },{ top: 16, left: 43 },{ top: 60, left: 68 }
+		{ top: 4, left: 30 },{ top: 7, left: 36 },{ top: 9, left: 5 },{ top: 12, left: 95 },{ top: 8, left: 90 },{ top: 14, left: 93 },{ top: 10, left: 12 },
+		{ top: 15, left: 20 },{ top: 40, left: 15 },{ top: 23, left: 12 },{ top: 28, left: 28 },{ top: 31, left: 34 },{ top: 31, left: 8 },{ top: 32, left: 87 },
+		{ top: 38, left: 65 },{ top: 46, left: 30 }, { top: 7, left: 65 },{ top: 13, left: 67 },{ top: 45, left: 98 },{ top: 34, left: 79 },{ top: 24, left: 66 },
+		{ top: 45, left: 84 },{ top: 16, left: 43 }
 	]
 	positions.forEach(function(p) {
 		var star = $('<img src="img/star.png" class="star">');
@@ -19,7 +21,7 @@ $(document).ready(function(){
 			'animation-duration': twinkle_period + 's'
 		});
 		star_container.append(star)
-	}); 
+	});
 
 
 	/**
@@ -45,7 +47,7 @@ $(document).ready(function(){
 					cloud.css("left","-20%");
 					setupCloudAnimation(cloud);
 				},100)
-				
+
 			}
 		});
 	}
@@ -85,4 +87,3 @@ $(document).ready(function(){
 	});
 
 });
-
