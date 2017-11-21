2bedd1b70ffeac66908e43ea97aa7b89348ac49a
Cory Crowley <ccrowley96@gmail.com>
2017-11-21 10:51:39 -0500

    Addressing pre-launch responsiveness bugs

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,39 @@
+var pipe = document.getElementById("pipe");
+//Change pipe to stack on mobile
+if (matchMedia) {
+  const mq = window.matchMedia("(min-width: 500px)");
+  mq.addListener(WidthChange);
+  WidthChange(mq);
+}
 
+// media query change
+function WidthChange(mq) {
+  if (mq.matches) {
+    // window width is at least 500px
+		pipe.innerHTML = "|";
+		console.log("pipe");
+  } else {
+    // window width is less than 500px
+		pipe.innerHTML = "<br>";
+		console.log("stack");
+  }
+
+}
 $(document).ready(function(){
+
 	// hide mlh badge when scrolling
 	var hidden = false;
 	$(document).scroll(function(){
-	    if($(this).scrollTop()>=$('#about_header').position().top){
+	    if($(this).scrollTop()>=$('#features-promo').position().top){
 	        if (!hidden) {
-	        	$("#mlh-trust-badge").fadeOut();
+	        	$("#mlh-trust-badge").fadeOut(800);
 	        	hidden = true;
 	   		}
 	    }else{
 	        if (hidden) {
-	        	$("#mlh-trust-badge").fadeIn();
+	        	$("#mlh-trust-badge").fadeIn('fast');
 	        	hidden = false;
 	        }
 	    }
 	});
-
 });
